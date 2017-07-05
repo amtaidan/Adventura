0ced15cf6b7e517f1981e9688f1c410e4aab27f9
Unknown <amtaidan@comcast.net>
2017-07-05 16:04:59 -0500

    4 Rhizopas. Rhizopa script.

---
+++ Assets/Scripts/RhizopaCombat.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class RhizopaCombat : MonoBehaviour {
+	public static int rhizHealth = 30;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
